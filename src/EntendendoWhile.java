e5f789c40b82ebffab473b5e21386b664bc48dcf
Joao Bizzarri <joaobizzarri@gmail.com>
2021-06-28 21:10:40 -0300

    Implementado o loop while

---
+++ src/EntendendoWhile.java
@@ -0,0 +1,13 @@
+
+public class EntendendoWhile {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while (a < 100) {
+			System.out.println("Esse é o oi de número " + a);
+			a = a + 1;
+		}
+
+	}
+
+}
